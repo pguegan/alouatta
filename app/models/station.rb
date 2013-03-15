cb97429cbf3a30a7f37c7449cab2ddd46e5accab
Philippe Guégan <philippe.guegan@gmail.com>
2013-03-15 14:26:16 +0100

    Ajout Rider-Radio, et meilleure prise en charge des erreurs de parsing des métadonnées

--- app/models/station.rb
+++ app/models/station.rb
@@ -11,7 +11,7 @@ class Station
   end
 
   def self.all
-    @@stations ||= %w{adidas classics hip-hop hits lounge myjungly pop-rock soul-funk une-autre-radio}.map { |name| Station.new(name.upcase) }
+    @@stations ||= %w{rider-radio adidas classics hip-hop hits lounge myjungly pop-rock soul-funk une-autre-radio}.map { |name| Station.new(name.upcase) }
   end
 
   def to_param
@@ -20,8 +20,10 @@ class Station
 
   def current_song
     document = Nokogiri::HTML(open('http://94.23.143.166:8000/title.xsl'))
-    data = document.xpath("//pre").first.content.scan(Regexp.new("#{@name.upcase}\\|\\|(.+)\\s-\\s(.+)")).first
+    data = document.xpath("//pre").first.content.scan(Regexp.new("#{@name}\\|\\|(.+)\\s-\\s(.+)")).first
     Song.new(data[0], data[1], "http://cdn.myjungly.fr/web/#{data[0]} - #{data[1]}.jpg")
+  rescue
+    Song.new("My Jungly Music", "Radios sur mesure", "http://cdn.myjungly.fr/web/My%20Jungly%20Music%20-%20Radios%20sur%20mesure.jpg")
   end
 
 end
