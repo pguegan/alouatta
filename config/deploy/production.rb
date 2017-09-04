0911f610c0d3414a775e8b057d658743c963b493
Philippe Guégan <philippe.guegan@gmail.com>
2017-09-04 19:19:56 +0200

    Downgrade Ruby 2.1.2

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,4 @@ set :branch,              ENV['GIT_BRANCH'] || "1.0-stable"
 set :user,                "alouatta_production"
 set :default_environment, { 'PATH' => "/home/#{user}/.rbenv/shims:/home/#{user}/.rbenv/bin:$PATH" }
 
-server                    "37.187.135.154", :web, :app, :db, primary: true
+server                    "178.62.52.20", :web, :app, :db, primary: true
